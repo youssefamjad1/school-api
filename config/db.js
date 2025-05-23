88a524ecc5f309fcaac32a8ff4f133fc6746ea80
Youssef HIMMI <yhimmi1@gmail.com>
2025-05-23 19:35:44 +0200

    update db config with hosted db param

--- config/db.js
+++ config/db.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,          // Add this line to specify port
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
