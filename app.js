0f6f7223bb286721d7bfcf3043d24299860c2e56
Youssef HIMMI <yhimmi1@gmail.com>
2025-05-23 21:09:00 +0200

    share postman collection

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 const schoolRoutes = require('./routes/schoolRoutes');
-
 const app = express();
 app.use(bodyParser.json());
 app.use('/', schoolRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
